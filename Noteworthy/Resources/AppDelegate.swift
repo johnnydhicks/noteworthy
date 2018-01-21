527a5ca6d3d8a648581acd326efeb556527afeb7
Johnny Hicks <johnnydhicks@gmail.com>
2018-01-21 14:45:16 -0700

    Bug Fixes - Start video on view will appear

--- Noteworthy/Resources/AppDelegate.swift
+++ Noteworthy/Resources/AppDelegate.swift
@@ -55,7 +55,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        let notification = Notification(name: Notification.Name(rawValue: "AppActive"), object: nil, userInfo: nil)
+        NotificationCenter.default.post(notification)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
